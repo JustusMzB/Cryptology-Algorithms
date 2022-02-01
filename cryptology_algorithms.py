246c716f8ac3d6e516bc1025fac8b2ac9cfb6fb2
JustusMzB <ju@mzb.de>
2022-02-01 12:05:04 +0100

    Additional Documentation

--- cryptology_algorithms.py
+++ cryptology_algorithms.py
@@ -1,3 +1,5 @@
 from extended_euclidian import extended_euclidian, modulo_inverse
 from pseudobinary_potences import binary_potencing
 
+"""This is a parent module. It features finished algorithms for quick import.
+"""
